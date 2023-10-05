76c4f6bc0302f4115e779ed10feb9df99aff27cd
suhaspete <petesuhas@gmail.com>
2023-10-05 19:19:39 +0530

    added gotop button

--- components/Footer/Footer.jsx
+++ components/Footer/Footer.jsx
@@ -3,6 +3,7 @@ import React from "react";
 import { Container, Row, Col } from "reactstrap";
 import classes from "./footer.module.css";
 import Link from "next/link";
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 
 const Footer = () => {
   const date = new Date();
@@ -17,6 +18,14 @@ const Footer = () => {
               <p>
                 &copy; Copyright {year} - Developed by Piyush Garg. All right
                 reserved.
+              <body>
+                <section>
+
+                </section>
+                <a class="gotopbtn" href="#"> <i class="fa-solid fa-arrow-up" > </i> </a>
+              </body>
+                
+              
               </p>
             </div>
           </Col>
